99d88aa90eba71d7de567371d971a2dbdb23e1ed
Carlos Daniel Morillo <109831057+Buf3r@users.noreply.github.com>
2023-01-02 19:52:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const boton = document.querySelector("#botn");
+const body = document.querySelector("#cuerpo");
+
+boton.addEventListener("click", e =>{
+  body.classList.toggle("oscuro");
+  
+    
+})
